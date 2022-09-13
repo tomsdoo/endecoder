d7ba81aa57f02c192eb1f072f921aabcd7f6a6a6
tom <toms@live.jp>
2022-09-14 05:45:13 +0900

    format script is executed

--- src/secretkey.ts
+++ src/secretkey.ts
@@ -11,155 +11,99 @@ class SecretKeyBase {
   protected algorithm: string;
   protected password: string;
   protected salt: string;
-  constructor(
-    algorithm: string,
-    password: string,
-    salt: string
-  ){
+  constructor(algorithm: string, password: string, salt: string) {
     this.algorithm = algorithm;
     this.password = password;
     this.salt = salt;
   }
-  protected getKey(){
+  protected getKey() {
     return crypto.scryptSync(this.password, this.salt, 32);
   }
-  public encode(data: string){
+  public encode(data: string) {
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(
-      this.algorithm,
-      this.getKey(),
-      iv
-    );
+    const cipher = crypto.createCipheriv(this.algorithm, this.getKey(), iv);
     let encryptedData = cipher.update(data);
-    encryptedData = Buffer.concat([
-      encryptedData,
-      cipher.final()
-    ]);
-    return [
-      iv.toString("base64"),
-      encryptedData.toString("base64")
-    ].join(":");
+    encryptedData = Buffer.concat([encryptedData, cipher.final()]);
+    return [iv.toString("base64"), encryptedData.toString("base64")].join(":");
   }
-  public decode(data: string){
-    const [
-      iv,
-      encryptedData
-    ] = data.split(":").map(
-      v => Buffer.from(v, "base64")
-    );
-    const decipher = crypto.createDecipheriv(
-      this.algorithm,
-      this.getKey(),
-      iv
-    );
+  public decode(data: string) {
+    const [iv, encryptedData] = data
+      .split(":")
+      .map((v) => Buffer.from(v, "base64"));
+    const decipher = crypto.createDecipheriv(this.algorithm, this.getKey(), iv);
     let decryptedData = decipher.update(encryptedData);
-    decryptedData = Buffer.concat([
-      decryptedData,
-      decipher.final()
-    ]);
+    decryptedData = Buffer.concat([decryptedData, decipher.final()]);
     return decryptedData.toString();
   }
-  public async encrypt(data: Buffer){
-    const deflatedData = await new Promise(
-      (resolve, reject) => deflate(data, (err, rdata) => (err ? reject : resolve)(err || rdata))
+  public async encrypt(data: Buffer) {
+    const deflatedData = await new Promise((resolve, reject) =>
+      deflate(data, (err, rdata) => (err ? reject : resolve)(err || rdata))
     );
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(
-      this.algorithm,
-      this.getKey(),
-      iv
-    );
+    const cipher = crypto.createCipheriv(this.algorithm, this.getKey(), iv);
     return [
       iv.toString("base64"),
       Buffer.concat([
         cipher.update(deflatedData as Buffer),
-        cipher.final()
-      ]).toString("base64")
+        cipher.final(),
+      ]).toString("base64"),
     ].join(":");
   }
-  public async decrypt(data: string): Promise<Buffer>{
-    const [
-      iv,
-      encryptedData
-    ] = data.split(":").map(
-      v => Buffer.from(v, "base64")
-    );
-    const decipher = crypto.createDecipheriv(
-      this.algorithm,
-      this.getKey(),
-      iv
-    );
-    return await new Promise(
-      (resolve, reject) => inflate(
-        Buffer.concat([
-          decipher.update(encryptedData),
-          decipher.final()
-        ]),
+  public async decrypt(data: string): Promise<Buffer> {
+    const [iv, encryptedData] = data
+      .split(":")
+      .map((v) => Buffer.from(v, "base64"));
+    const decipher = crypto.createDecipheriv(this.algorithm, this.getKey(), iv);
+    return await new Promise((resolve, reject) =>
+      inflate(
+        Buffer.concat([decipher.update(encryptedData), decipher.final()]),
         (err, rdata) => {
-          if(err){
+          if (err) {
             reject(err);
-          }else{
+          } else {
             resolve(rdata);
           }
         }
       )
     );
   }
-  public static activate({
-    algorithm,
-    password,
-    salt
-  }: SecretKeyOptions){
-    return new this(
-      algorithm as string,
-      password as string,
-      salt as string
-    );
+  public static activate({ algorithm, password, salt }: SecretKeyOptions) {
+    return new this(algorithm as string, password as string, salt as string);
   }
 }
 
 export class SecretKey extends SecretKeyBase {
-  constructor(options?: SecretKeyOptions){
+  constructor(options?: SecretKeyOptions) {
     const defaultKeys = SecretKey.generateKeys();
     super(
-      options && options.algorithm || defaultKeys.algorithm,
-      options &&options.password || defaultKeys.password,
-      options && options.salt || defaultKeys.salt
+      (options && options.algorithm) || defaultKeys.algorithm,
+      (options && options.password) || defaultKeys.password,
+      (options && options.salt) || defaultKeys.salt
     );
   }
-  public static activate(options?: SecretKeyOptions){
+  public static activate(options?: SecretKeyOptions) {
     return new this(options);
   }
-  public static generateKeys(){
+  public static generateKeys() {
     return {
       algorithm: "aes-256-cbc",
       password: crypto.randomBytes(48).toString("base64"),
-      salt: crypto.randomBytes(12).toString("base64")
+      salt: crypto.randomBytes(12).toString("base64"),
     };
   }
 }
 
-export function encode(
-  data: string,
-  options?: SecretKeyOptions
-){
+export function encode(data: string, options?: SecretKeyOptions) {
   const lopt = {
     ...SecretKey.generateKeys(),
-    ...(options ? options : {})
+    ...(options ? options : {}),
   };
   return {
-    data: SecretKey
-      .activate(lopt)
-      .encode(data),
-    options: lopt
+    data: SecretKey.activate(lopt).encode(data),
+    options: lopt,
   };
 }
 
-export function decode(
-  data: string,
-  options?: SecretKeyOptions
-){
-  return SecretKey
-    .activate(options)
-    .decode(data);
+export function decode(data: string, options?: SecretKeyOptions) {
+  return SecretKey.activate(options).decode(data);
 }
--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import {describe, it } from "mocha";
+import { describe, it } from "mocha";
 import { strict as assert } from "assert";
 import { randomBytes } from "crypto";
 
@@ -22,9 +22,9 @@ describe("SecretKey", () => {
 
   it("decode() - 2", () => {
     const encoded1 = key1.encode(plain_text);
-    try{
+    try {
       assert(key2.decode(encoded1) != plain_text);
-    }catch(e){
+    } catch (e) {
       assert(e instanceof Error);
     }
   });
@@ -35,18 +35,12 @@ describe("SecretKey", () => {
 
   it("encode()", () => {
     const result = encode(plain_text);
-    assert(
-      "data" in result &&
-      "options" in result
-    );
+    assert("data" in result && "options" in result);
   });
 
   it("decode()", () => {
     const result = encode(plain_text);
-    assert.equal(
-      decode(result.data, result.options),
-      plain_text
-    );
+    assert.equal(decode(result.data, result.options), plain_text);
   });
 
   it("encrypt() & decrypt()", async () => {
@@ -57,9 +51,6 @@ describe("SecretKey", () => {
       plainBuffer.toString("base64"),
       decryptedData.toString("base64")
     );
-    assert.rejects(
-      () => key2.decrypt(encryptedData)
-    );
+    assert.rejects(() => key2.decrypt(encryptedData));
   });
-
 });
